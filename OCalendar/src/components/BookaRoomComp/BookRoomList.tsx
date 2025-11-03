48f5018c32db6e0fab28f6c7e69c9a2638f75ad3
rydbl <159252313+rydbl@users.noreply.github.com>
2025-11-03 13:38:51 +0100

    book room list updatte

---
+++ OCalendar/src/components/BookaRoomComp/BookRoomList.tsx
@@ -0,0 +1,16 @@
+function BookRoomList({ bookedRooms } : { bookedRooms: any[] }) {
+    return (
+        <div className="BookRoomList__container">
+            {bookedRooms.map((booking: any, index: number) => (
+                <div key={index} className="BookRoomList__item">
+                    <p>
+                        Room: {booking.roomName}
+                        {/* Booked By: {booking.bookedBy}
+                        Date: {booking.date} */}
+                    </p>
+                </div>
+            ))}
+        </div> 
+    );
+}
+export default BookRoomList;
--- OCalendar/src/components/BookedRooms.tsx
+++ OCalendar/src/components/BookedRooms.tsx
@@ -1,12 +1,22 @@
+import '../stylesheets/BookedRooms.css';
+import BookRoomList from './BookaRoomComp/BookRoomList';
+
+const dummyBookedRooms = [
+  { id: '1', roomName: 'Atlas', roomNumber: 'A101', location: '1st floor', timeSlot: '09:00 - 10:00', bookedBy: 'Alice' },
+  { id: '2', roomName: 'Orion', roomNumber: 'B202', location: '2nd floor', timeSlot: '11:00 - 12:00', bookedBy: 'Bob' },
+  { id: '3', roomName: 'Vega', roomNumber: 'C303', location: '3rd floor', timeSlot: '14:00 - 15:00', bookedBy: 'Carol' },
+];
+
 function BookedRooms() {
     return (
         <div className="BookedRooms__container">
             <h3>Booked Rooms</h3>
-            <ul className="BookedRooms__list">
-                <li className="BookedRooms__item">Room 1 - 2024-07-10</li>
-                <li className="BookedRooms__item">Room 2 - 2024-07-12</li>
-                <li className="BookedRooms__item">Room 3 - 2024-07-14</li>
-            </ul>
+            <div className="BookedRooms__underline"></div>
+
+            {/** component to list booked rooms will go here */}
+            <BookRoomList bookedRooms={dummyBookedRooms} />
+
+
         </div>
     );
 }
