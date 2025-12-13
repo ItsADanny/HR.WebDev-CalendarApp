1ac1368dccf6f374ed7d125714f9a1cc9235d734
ItsDanny <141739582+ItsADanny@users.noreply.github.com>
2025-12-13 16:15:11 +0100

    Changed BookingService to increase added data to message

--- OCalendar-API/Controllers/RoomBookingController.cs
+++ OCalendar-API/Controllers/RoomBookingController.cs
@@ -5,48 +5,86 @@ using Microsoft.AspNetCore.Mvc;
 public class RoomBookingController : ControllerBase
 {
     private readonly IRoomBookingService _RoomBookingService;
+    private readonly IRoomService _roomService;
+    private readonly ITimeslotService _timeslotService;
 
-    public RoomBookingController(IRoomBookingService roomBookingService)
+    public RoomBookingController(IRoomBookingService roomBookingService, IRoomService roomService, ITimeslotService timeslotService)
     {
         _RoomBookingService = roomBookingService;
+        _roomService = roomService;
+        _timeslotService = timeslotService;
     }
 
     // ====================================================================================
     // GET
     // ====================================================================================
     [HttpGet("")]
-    public ActionResult<IEnumerable<Timeslot>> GetAll() => Ok(_RoomBookingService.GetAll());
+    public ActionResult<IEnumerable<RoomBooking>> GetAll() {
+        IEnumerable<RoomBooking> bookings = _RoomBookingService.GetAll();
+
+        foreach (RoomBooking booking in bookings)
+        {
+            booking.Room = _roomService.GetByID((int) booking.RoomId);
+            booking.Timeslot = _timeslotService.GetByID((int) booking.TimeslotId);
+        }
+
+        return Ok(bookings);
+    }
 
     [HttpGet("{id:int}")]
-    public ActionResult<EventAttendance> GetByID(int id)
+    public ActionResult<RoomBooking> GetByID(int id)
     {
         RoomBooking? foundRoomBooking = _RoomBookingService.GetByID(id);
         if (foundRoomBooking == null) return NotFound();
+
+        foundRoomBooking.Room = _roomService.GetByID((int) foundRoomBooking.RoomId);
+        foundRoomBooking.Timeslot = _timeslotService.GetByID((int) foundRoomBooking.TimeslotId);
         
         return Ok(foundRoomBooking);
     }
 
     [HttpGet("room/{roomId:int}")]
-    public ActionResult<IEnumerable<Timeslot>> GetByRoom(int roomId)
+    public ActionResult<IEnumerable<RoomBooking>> GetByRoom(int roomId)
     {
         IEnumerable<RoomBooking>? foundTimeslot = _RoomBookingService.GetByRoom(roomId);
         if (foundTimeslot == null) return NotFound();
+
+        foreach (RoomBooking booking in foundTimeslot)
+        {
+            booking.Room = _roomService.GetByID((int) booking.RoomId);
+            booking.Timeslot = _timeslotService.GetByID((int) booking.TimeslotId);
+        }
+
         return Ok(foundTimeslot);
     }
 
     [HttpGet("timeslot/{timeslotId:int}")]
-    public ActionResult<IEnumerable<Timeslot>> GetByDate(int timeslotId)
+    public ActionResult<IEnumerable<RoomBooking>> GetByDate(int timeslotId)
     {
         IEnumerable<RoomBooking>? foundTimeslot = _RoomBookingService.GetByTimeslot(timeslotId);
         if (foundTimeslot == null) return NotFound();
+
+        foreach (RoomBooking booking in foundTimeslot)
+        {
+            booking.Room = _roomService.GetByID((int) booking.RoomId);
+            booking.Timeslot = _timeslotService.GetByID((int) booking.TimeslotId);
+        }
+
         return Ok(foundTimeslot);
     }
 
     [HttpGet("user/{userId:int}")]
-    public ActionResult<IEnumerable<Timeslot>> GetByName(int userId)
+    public ActionResult<IEnumerable<RoomBooking>> GetByName(int userId)
     {
         IEnumerable<RoomBooking>? foundRoomBooking = _RoomBookingService.GetByUser(userId);
         if (foundRoomBooking == null) return NotFound();
+
+        foreach (RoomBooking booking in foundRoomBooking)
+        {
+            booking.Room = _roomService.GetByID((int) booking.RoomId);
+            booking.Timeslot = _timeslotService.GetByID((int) booking.TimeslotId);
+        }
+
         return Ok(foundRoomBooking);
     }
 
@@ -54,7 +92,7 @@ public class RoomBookingController : ControllerBase
     // POST
     // ====================================================================================
     [HttpPost("")]
-    public ActionResult<Timeslot> Create([FromBody] RoomBookingDto rb)
+    public ActionResult<RoomBooking> Create([FromBody] RoomBookingDto rb)
     {
         return Ok(_RoomBookingService.Create(rb));
     }
@@ -63,7 +101,7 @@ public class RoomBookingController : ControllerBase
     // PUT
     // ====================================================================================
     [HttpPut("{id:int}")]
-    public ActionResult<Timeslot> Update(int id, [FromBody] RoomBookingDto rb)
+    public ActionResult<RoomBooking> Update(int id, [FromBody] RoomBookingDto rb)
     {
         RoomBooking? roomBookingSucces = _RoomBookingService.Update(id, rb);
         return roomBookingSucces == null ? NotFound() : Ok(roomBookingSucces);
@@ -73,5 +111,5 @@ public class RoomBookingController : ControllerBase
     // DELETE
     // ====================================================================================
     [HttpDelete("{id:int}")]
-    public ActionResult<Timeslot> Delete(int id) => _RoomBookingService.Delete(id) ? NotFound() : Ok();
+    public ActionResult<RoomBooking> Delete(int id) => _RoomBookingService.Delete(id) ? NotFound() : Ok();
 }
