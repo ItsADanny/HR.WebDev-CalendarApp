1e47716d04416b90e4107ba0cacba8a6f1bb9fa1
rydbl <159252313+rydbl@users.noreply.github.com>
2026-01-05 23:08:01 +0100

    Home opschonen

--- OCalendar/src/pages/Home.tsx
+++ OCalendar/src/pages/Home.tsx
@@ -6,18 +6,15 @@ function Home() {
       <>
           <h1>Welcome to the Team Orange Calendar!</h1>
           <p>This is the main landing page of our application.</p>
+
           <HomeCounter />
+
           <Link to="/login">Go to Login Page</Link>
           <br />
           <Link to="/register">Go to Register Page</Link>
           <br />
-          <Link to="/book-a-room">Book a room!</Link>
-          <br />
-          <Link to="/attending">Attending Page</Link>
-          <br />
           <Link to="/admin-dashboard">Admin Dashboard</Link>
 
-          <HomeTimer />
       </>
     )
 }
@@ -34,29 +31,12 @@ function HomeCounter() {
     localStorage.setItem('counter', counter.toString());
   }, [counter]);
 
-  const [color, setColor] = useState<string>('orange');
-
   return (
     <div>
       <h1>Orange Count: {counter}</h1>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
       <button onClick={() => setCounter(counter - 1)}>Decrement</button>
-      <button 
-        type="button" 
-        onClick={() => setColor(color === 'orange' ? 'blue' : 'orange')}
-        style={{backgroundColor: color}}
-      >Toggle Color</button>
+
     </div>
   )
-}
-
-function HomeTimer() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  });
-  return <h1>I've rendered {count} times!</h1>;
 }
